541960dd48e49c7eb3f481aca079cbecb01539b5
mkreive <mkreive@gmail.com>
2022-05-10 10:07:49 +0300

    html: login form and button added

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 "use strict";
 
 // DATA
+const dummyLists = [
+    {
+        user: "Monika",
+        password: "monkey",
+        toDoList: [
+            "Walk dogs to the forest",
+            "Do yoga",
+            "Play a board game with family",
+            "Read a book",
+        ],
+        doneList: ["Complete React online course", "Do dishes"],
+    },
+    {
+        user: "Jonas",
+        password: "jonas",
+        toDoList: ["Buy apartment", "Go outside", "Play a board game"],
+        doneList: ["Finish project at work", "Feed the fish"],
+    },
+];
 
 // SELECTORS
 
