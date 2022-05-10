a2ae161a25c1286f06a171db02ee6d3b28a9422f
mkreive <mkreive@gmail.com>
2022-05-10 11:08:08 +0300

    style: login form styling done

--- script.js
+++ script.js
@@ -22,9 +22,31 @@ const dummyLists = [
 ];
 
 // SELECTORS
+// buttons
+const loginBtn = document.querySelector(".header__login");
+const themeSwitchBtn = document.querySelector(".header__theme");
+const dragAndDrop = document.querySelector(".bottom");
+
+// overlays
+const loginOverlay = document.querySelector(".login");
+const overlay = document.querySelector(".overlay");
+
+// other
+const inputField = document.querySelector(".text__input");
+const listItems = document.querySelectorAll(".card__item");
+const listItemCounterEl = document.querySelector(".items-left");
 
 // VARIABLES
 
 // HELPER FUNCTIONS
 
 // EVENT LISTENERS
+loginBtn.addEventListener("click", function () {
+    loginOverlay.showModal();
+    overlay.classList.remove("hidden");
+});
+
+overlay.addEventListener("click", function () {
+    loginOverlay.closest();
+    overlay.classList.add("hidden");
+});
